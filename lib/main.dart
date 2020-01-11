09acb4ed3f9af7a8a3beb6cd6ba593ba4233593d
Naxalov <uzbzarif@gmail.com>
2020-01-11 21:02:11 +0900

    Login page

--- lib/main.dart
+++ lib/main.dart
@@ -20,18 +20,57 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       child: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            Text("Logo"),
-            Text("Username"),
-            Text("Password"),
-            RaisedButton(
-              child: Text("Login"),
-              onPressed: () {},
-            )
-          ],
+        child: Center(
+
+          child: Column(
+
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
+            children: <Widget>[
+              Image.asset(
+                'images/avatar.png',
+                scale: 5,
+              ),
+              Text("Logo"),
+
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.account_box),
+                  hintText: 'Username',
+                  labelText: 'Username',
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  icon: const Icon(Icons.lock_outline),
+                  hintText: 'password',
+                  labelText: 'password',
+                ),
+                obscureText: true,
+                keyboardType: TextInputType.visiblePassword,
+              ),
+              Container(
+                margin: EdgeInsets.all(10),
+                child: FlatButton(
+                  color: Colors.blue,
+                  textColor: Colors.white,
+                  disabledColor: Colors.grey,
+                  disabledTextColor: Colors.black,
+                  
+                  padding: EdgeInsets.all(8.0),
+                  splashColor: Colors.blueAccent,
+                  onPressed: () {
+                    /*...*/
+                  },
+                  child: Text(
+                    "Flat Button",
+                    style: TextStyle(fontSize: 20.0),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
